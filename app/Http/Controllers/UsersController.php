ea672453287171c167fd7e51c60c9574e2ac91ec
QYS <653994784@qq.com>
2019-10-11 11:58:38 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
     	return view('users.show', compact('user'));
@@ -16,6 +21,7 @@ class UsersController extends Controller
 
     public function update(User $user, UserRequest $request, ImageUploadHandler $uploader)
     {
+    	$this->authorize('update', $user);
     	$data = $request->all();
     	if ($request->avatar) {
     		$result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
@@ -30,6 +36,7 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+    	$this->authorize('update', $user);
     	return view('users.edit', compact('user'));
     }
 }
