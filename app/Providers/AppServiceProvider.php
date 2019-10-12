fa72267ed9693464922165c81ba1eb317f25537c
QYS <653994784@qq.com>
2019-10-12 10:52:17 +0800

    修复 XSS 注入漏洞

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use App\Models\Topic;
+use App\Observers\UserObserver;
+use App\Observers\TopicObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::observe(UserObserver::class);
+        Topic::observe(TopicObserver::class);
     }
 }
